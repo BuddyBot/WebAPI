0290983e143e05bb2589727ef4e225aaaed8c69d
rivayama <tomozo16@gmail.com>
2014-11-13 16:14:23 +0000

    Add method "purchased?"

--- app/models/purchased_product.rb
+++ app/models/purchased_product.rb
@@ -1,4 +1,13 @@
 class PurchasedProduct < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+
+  def self.purchased?(product_id)
+    if all.select {|x| x.product_id == product_id}.empty? then
+      false
+    else
+      true
+    end
+  end
+
 end
