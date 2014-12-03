bedb0335b68451bc9e6b1aafdc972f4d30c49505
rivayama <tomozo16@gmail.com>
2014-12-03 11:29:16 +0000

    Use name to sign-up and sign-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :recoverable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
+  def email_required?
+    false
+  end
+
   has_many :purchased_products
   has_many :products, :through => :purchased_products
   validates :name, presence: true, uniqueness: true
