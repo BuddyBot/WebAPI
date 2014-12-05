b49bea6f9390250c4783b178069168d1268deedb
rivayama <tomozo16@gmail.com>
2014-12-05 14:01:03 +0000

    Add robot_id to sign-up and profile edit fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,5 @@ class User < ActiveRecord::Base
   has_many :purchased_products
   has_many :products, :through => :purchased_products
   validates :name, presence: true, uniqueness: true
+  validates :robot_id, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :missions
   resources :products
   post 'products/:id/purchase' => 'products#purchase', as: :purchase
